a53b173e4b100586a6b347108cea4edc32931019
Mickey0001 <mirzasisic57@gmail.com>
2019-04-15 22:43:30 +0200

    Project setup

--- WordPressAutomationFramework/Automation.cs
+++ WordPressAutomationFramework/Automation.cs
@@ -5,6 +5,10 @@ namespace WordPressAutomationFramework
 {
     public class Automation
     {
-
+        public void Go()
+        {
+            var driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://bing.com");
+        }
     }
 }
--- WordPressAutomationFramework/FirefoxDriver.cs
+++ WordPressAutomationFramework/FirefoxDriver.cs
@@ -1,9 +1,16 @@
-﻿namespace WordPressAutomationFramework
+﻿using System;
+
+namespace WordPressAutomationFramework
 {
     internal class FirefoxDriver
     {
         public FirefoxDriver()
         {
+        }
+
+        internal object Navigate()
+        {
+            throw new NotImplementedException();
         }
     }
 }
