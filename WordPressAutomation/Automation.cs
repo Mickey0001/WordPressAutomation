09ad22189ed9bda9db73c988f9410b91f8dff5de
Mickey0001 <mirzasisic57@gmail.com>
2019-04-16 22:41:57 +0200

    First test successful!

---
+++ WordPressAutomation/Automation.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordPressAutomation
+{
+    public class Automation
+    {
+        public void Go()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://bing.com/");
+        }
+    }
+}
