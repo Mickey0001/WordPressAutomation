2ea2965708f4433aeb39ace54826ccfeb86a8e69
Mickey0001 <mirzasisic57@gmail.com>
2019-04-17 22:19:55 +0200

    WordPress Login Test

---
+++ WordPressAutomation/Driver.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace WordPressAutomation
+{
+    public class Driver
+    {
+        public static IWebDriver Instance { get; set; }
+
+        public static void Initialize()
+        {
+            Instance = new ChromeDriver();
+            Instance.Manage().Timeouts().ImplicitWait.(TimeSpan.FromSeconds(5));
+        }
+    }
+}
---
+++ WordPressAutomation/LoginPage.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordPressAutomation
+{
+    public class LoginPage
+    {
+        public static void GoTo()
+        {
+            Driver.Instance.Navigate().GoToUrl("http://localhost/test/wp-login.php");
+            var wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
+            wait.Until(d => d.SwitchTo().ActiveElement().GetAttribute("id") == "user_login");
+        }
+
+        public static LoginCommand LoginAs(string userName)
+        {
+            return new LoginCommand(userName);
+        }
+    }
+
+    public class LoginCommand
+    {
+        private readonly string userName;
+        private string password;
+
+        public LoginCommand(string userName)
+        {
+            this.userName = userName;
+        }
+
+        public object WithPassword(string password)
+        {
+            this.password = password;
+            return this;
+        }
+
+        public void Login()
+        {
+            var LoginInput = Driver.Instance.FindElement(By.Id("user_login"));
+            LoginInput.SendKeys(userName);
+
+            var PasswordInput = Driver.Instance.FindElement(By.Id("user_pass"));
+            PasswordInput.SendKeys(password);
+
+            var LoginButton = Driver.Instance.FindElement(By.Id("wp-submit"));
+            LoginButton.Click();
+        }
+    }
+}
