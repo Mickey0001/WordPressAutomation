4d2beff493be0040307be093ab3f56acbca5dc64
Mickey0001 <mirzasisic57@gmail.com>
2019-05-06 22:23:23 +0200

    Login Page updates

---
+++ WordPressAutomationFramework/LoginPage.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordPressAutomationFramework
+{
+    public class LoginPage
+    {
+        public static void GoTo()
+        {
+            var driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("http://localhost/test/wp-login.php");
+        }
+
+        public static LoginCommand LoginAs(string UserName)
+        {
+            return new LoginCommand(UserName);
+        }
+    }
+
+    public class LoginCommand
+    {
+        private readonly string userName;
+        private string password;
+
+        public LoginCommand(string userName)
+        {
+            this.userName = userName;
+        }
+
+        public void Login()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LoginCommand WithPassword(string password)
+        {
+            this.password = password;
+            return this;
+        }
+    }
+}
---
+++ WordPressTests/LoginTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordPressAutomationFramework;
+
+namespace WordPressTests
+{
+    [TestClass]
+    public class LoginTests
+    {
+        [TestMethod]
+        public void AdminUserCanLogin()
+        {
+            LoginPage.GoTo();
+            LoginPage.LoginAs("test").WithPassword("password").Login();
+
+            Assert.IsTrue(DashboardPage.IsAt, "Failed to login as admin");
+        }
+    }
+}
