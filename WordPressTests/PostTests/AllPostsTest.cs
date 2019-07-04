2ec39b1a4943388b26daf9266e7851674c9a059c
Mickey0001 <mirzasisic57@gmail.com>
2019-07-04 20:26:14 +0200

    Refactoring the framework

---
+++ WordPressTests/PostTests/AllPostsTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordPressTests.PostTests
+{
+    [TestClass]
+    public class AllPostsTests : WordPressTest
+    {
+        //Added posts show up in all posts
+        //Can activate excerpt mode
+        //Can add new post
+
+        //Single post selections
+
+        //Can select a post by title
+        //Can select a post by Edit
+        //Can select a post by QuickEdit
+        //Can trash a post
+        //Can view a post
+        //Can filter by auther
+        //Can filter by category
+        //Can filter by tag
+        //Can go to post comments
+
+        //Bulk actions
+
+        //Can edit multiple posts
+        //Can trash multiple posts
+        //Can select all posts
+
+        //Can search posts
+
+        //Added posts show up in all posts
+        //Can Trash a post
+        //Can search post
+
+        [TestMethod]
+        public void AddedPostsShowUp()
+        //Go to posts, get post count, store
+
+        //Add new post
+
+        //Go to posts, get new posts count
+
+        //Check for added post
+
+        //Trash post (clean up)
+    }
+}
